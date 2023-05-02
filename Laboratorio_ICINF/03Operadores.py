31bf6ebc9cb223178e97a7c96cee83418ddd4d7f
Boris1409 <borisytoby2004@gmail.com>
2023-05-02 17:12:20 -0400

    Clase 02-05-2023. Operadores

--- Laboratorio_ICINF/03Operadores.py
+++ Laboratorio_ICINF/03Operadores.py
@@ -1,22 +1,31 @@
+"""03-Operadores"""
+
+"""--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"""
+
 bencina = True
 encendido = True
 edad = 19
 
-if bencina and encendido: #El and tienen que cumplirse ambas condiciones
+if bencina and encendido:  #"""El and tienen que cumplirse ambas condiciones"""
     print("El vehiculo puede avanzar")
 else:
     print("El vehiculo no puede arrancar")
-    
+
+print("\n")
+
 if bencina  or encendido: #El or basta con que se cumpla una condicion 
     print("El vehiculo puede correr")
 else:
     print("El vehiculo no puede arrancar")
     
+print("\n")
 
 if not bencina and encendido: #El not es una contradiccion, si es true = false y si es false = true 
     print("")
 else:
     print("El vehiculo no puede arrancar")
+    
+print("\n")
     
 if not bencina or (encendido and edad >= 99):
     print("Esta maquina correra")
