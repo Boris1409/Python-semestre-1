495d1abf3d9b2101d10075bddcb01a1deedec2ef
Boris1409 <borisytoby2004@gmail.com>
2023-06-02 14:45:14 -0400

    Reto clase 7

--- Laboratorio_ICINF/reto_clase7.py
+++ Laboratorio_ICINF/reto_clase7.py
@@ -1,9 +1,17 @@
-def reto_clase7(escritura):
-    escitura_en_diccionario = {}
-    escritura = str(input("Escriba una frase: "))
-    escritura.count(escritura)
-    for rango in escritura:
-        escitura_en_diccionario(rango) = escritura.count(rango)
-        return print(escritura_en_diccionario)
-
+def reto_clase7(enunciado):
+    dic_de_palabras = {}
+    conteo_palabras = enunciado.split()
+    for palabra in conteo_palabras:
+        dic_de_palabras[palabra] = len(palabra)
+        return dic_de_palabras
     
+while True:
+    frase = input("Ingrese una frase, y y le diremos cuantas palabras contiene y su longitud: ")
+    if frase.isdigit():
+        print("No se aceptan numeros, ingrese de nuevo")
+        continue
+    else:
+        break
+    
+resultado = reto_clase7(frase)
+print(resultado)
