c508e2c3cc1d25486bac53f0d3a8b05cc2ce8f26
Boris1409 <borisytoby2004@gmail.com>
2023-06-13 00:19:53 -0400

    CORAZON DE MI ATO

---
+++ Comandos_PYTHON/ato_corazon.py
@@ -0,0 +1,39 @@
+while True:
+    print("""\n
+      
+       ______    ______
+      /......\  /......\ 
+     /.°°°°°°.\/.°°°°°°.\       
+    /.°°°°°°°°..°°°°°°°°.\ 
+    \.°- TE AMO KAREN -°./
+     \.°°°°°° <3 °°°°°°./
+      \.°°°°°°°°°°°°°°./
+       \.°°°°°°°°°°°°./
+        \.°°°°°°°°°°./
+         \.°°°°°°°°./
+          \.°°°°°°./
+           \.°°°°./
+            \.°°./
+             \../
+              \/     
+       
+       
+       
+       
+       
+       
+       la mas qlona uwu
+      """)
+    
+    name = input("Pone tu nombre camavinga: ")
+    if name.isdigit():
+        print(' No se aceptan numero webon')
+        continue
+    else:
+        if name.capitalize() == 'Karen':
+            print(' SOS LA MAS HERMOSA Y DIVINA DE TUDU MUNDU MUNDIALEEEEE :D')
+        elif name.capitalize() == 'Boris':
+            print(' FUAA SOS TERRIBLE RIKOO')
+        else:
+            print(' ERI TERRIBLE GEI JAJAJAJAJAJA')
+    break
